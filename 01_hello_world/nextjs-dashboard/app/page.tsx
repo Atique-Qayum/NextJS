71227fe44b97d717e4d9907366ac14d3aa37d566
muhammadatiqueqayum <atiqueqayum@gmail.com>
2024-03-18 23:53:00 +0500

    add initial file

--- 01_hello_world/nextjs-dashboard/app/page.tsx
+++ 01_hello_world/nextjs-dashboard/app/page.tsx
@@ -5,16 +5,27 @@ import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 import styles from '@/app/ui/home.module.css';
 <div className={styles.shape} />;
-<div
-  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
-/>
+
 
 
 export default function Page() {
   return (
     // ...
+    
     <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+    <div
+  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+/>
       {/* Add Hero Images Here */}
+    <p
+      className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+    >
+      <strong>Welcome to Acme.</strong> This is the example for the{' '}
+      <a href="https://nextjs.org/learn/" className="text-blue-500">
+        Next.js Learn Course
+      </a>
+      , brought to you by Vercel.
+    </p>
       <Image
         src="/hero-desktop.png"
         width={1000}
@@ -22,6 +33,8 @@ export default function Page() {
         className="hidden md:block"
         alt="Screenshots of the dashboard project showing desktop version"
       />
+      <AcmeLogo />
+      
       
     </div>
     //...
